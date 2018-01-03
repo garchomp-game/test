e7b625f3035406f5bbe293aef747678b49a028a7
garchomp-game <otoka.japanesedrum@gmail.com>
2018-01-04 01:07:07 +0900

    first-commit

---
+++ hoge.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>hoge</title>
+    <link rel="stylesheet" href="./hoge.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1><?php echo 'hoge' ?></h1>
+        <ul>
+          <li><a href="#">hoge</a></li>
+          <li><a href="#">hoge</a></li>
+          <li><a href="#">hoge</a></li>
+        </ul>
+      </div>
+    </header>
+  </body>
+</html>
